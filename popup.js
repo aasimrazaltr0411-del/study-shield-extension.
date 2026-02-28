3ea1ebc0cd91cf95885e46b8ae7072b453e96c14
aasimrazaltr0411-del <aasimrazaltr0411@gmail.com>
2026-02-28 12:58:06 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,6 @@
-
+document.getElementById('saveBtn').onclick = () => {
+  const text = document.getElementById('syllabusInput').value;
+  chrome.storage.local.set({ mySyllabus: text }, () => {
+    alert("Saved! Ab padhai shuru karein.");
+  });
+};
